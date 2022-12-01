b08a19d184b0239c8e9394b28e1dd72983ef8fea
Jonathan-Rezende <jonathanrezende1997@gmail.com>
2022-12-01 13:36:10 -0300

    headerinit

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -2,6 +2,30 @@ import styled from "styled-components"
 
 export const HeaderContainer = styled.header`
     height: 90px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
     background-color: black;
+    
+    div{
+        flex: 1;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        color:white;
+
+        .header-ul{
+        width:100%;
+        height: 100%;
+        border: 1px solid red;
+        display: flex;
+        align-items: center;
+        justify-content: space-around;
+        }
+    }
+
+
+
 `;
 
